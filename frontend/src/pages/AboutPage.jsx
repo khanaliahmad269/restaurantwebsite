b8702d90cf237e8b7597e0019a5d1b106c529a25
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-12 11:29:28 +0500

    Add frontend/src/pages/AboutPage.jsx

---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutContent from '../components/AboutContent';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <AboutContent />
+    </div>
+  );
+};
+
+export default AboutPage;
